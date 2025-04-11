6108ea5e80abbb8c0ee72c9044e4a7cf134400e0
i eat sand <xtema5002x@gmail.com>
2025-04-11 10:19:56 +0300

    ledora finux

--- src/input_handler.c
+++ src/input_handler.c
@@ -15,12 +15,12 @@ void handle_input(packed_string_t* input) {
         bt_update_terminal_size();
         *input = 0;
     }
-    else if (' ' <= *input && *input <= 0x7F) {
-        bt_log(BT_LOG_DEBUG, "input: '%c'", *input);
+    else if (' ' <= ch && ch <= 0x7F) {
+        bt_log(BT_LOG_DEBUG, "input: '%c'", ch);
         *input = BT_PACK_STRING_CHAR(ch);
     }
     else {
-        bt_log(BT_LOG_DEBUG, "input: '\\0x%02X'", (unsigned char)*input);
+        bt_log(BT_LOG_DEBUG, "input: '\\0x%X'", ch);
         *input = ch;
     }
 }
